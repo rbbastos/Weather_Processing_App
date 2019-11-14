0406329909ba8e32b5d6d94c32f7e24bd9e0a927
rbbastos <bastoscanada@gmail.com>
2019-11-14 14:34:19 -0600

    Scraper_Weather file implemented. File is generating a Dictionary with all data.

--- scrape_weather.py
+++ scrape_weather.py
@@ -11,9 +11,9 @@ class WeatherScraper(HTMLParser):
     def __init__(self):
         """Create an instance of WeatherScraper."""
         HTMLParser.__init__(self)
-        self.inTr = self.inTh = self.inAabr = self.inA = self.inTd = self.tbody = self.isDate = False
-        self.inMean = self.inMin = self.inMax = self.inAvg = False
-        self.i = 0
+        self.inTr = self.inTh = self.inAabr = self.inA = self.inTd = self.isDate = False
+        self.inMean = self.inMin = self.inMax = self.inAvg = self.inMyDate = False
+        self.i = self.j = 0
         self.keys = ["max", "min", "mean"]
         self.dictInner = {}
         self.dictOuter = {}
@@ -21,44 +21,52 @@ class WeatherScraper(HTMLParser):
 
     def handle_starttag(self, tag, attrs):
         """Handle the starttag in a website."""
-        # print("Start tag:", tag)
-        # for attr in attrs:
-        #     print("     attr:", attr)
+
         if tag == 'tr':
+            """Only data in table - total of 33 (depending of number of days in month, it will vary)"""
             try:
                 self.inTr = True
             except Exception as e:
                 print("Error:", e)
+
         if tag == 'td':
+            """First td is first temp in the table, under column MAX and DAY 1"""
             try:
-                # print("Start tag:", tag)
+                """i = 1 -> column MAX TEMP; i = 2 -> column MIN TEMP; i = 3 -> column MEAN TEMP"""
                 self.i += 1
                 self.inTd = True
             except Exception as e:
                 print("Error:", e)
+
         if tag == 'th':
+            """First th = DAY(0,0) in table. Date is inside a td>th>abbr(attr[1]). Total: 43 """
             try:
                 self.inTh = True
             except Exception as e:
                 print("Error:", e)
+
         if tag == 'abbr':
+            """Date is located in here, attr[1]. Total: 62"""
             try:
                 self.inAabr = True
             except Exception as e:
                 print("Error:", e)
-            for attr in attrs:
-                self.inMean = True
-                if self.inTh and not self.inTd:
-                    print("     attrrrrr:", attr[1])
-                    try:
-                        # https://stackoverflow.com/questions/42980662/convert-string-with-month-name-to-datetime
-                        # https://docs.python.org/2/library/datetime.html#strftime-and-strptime-behavior
-                        #print(datetime.datetime.strptime(attr[1], '%B %d, %Y').date())
+
+        for attr in attrs:
+            """attrs are inside a tag. E.g id='test'."""
+            self.inMean = True
+            if self.inTh and self.inAabr and attr[0] == 'title':
+                if attr[1] == 'kilometres per hour':
+                    self.inMyDate = True
+                # print("     attrrrrr:", attr[1])
+                try:
+                    if self.inMyDate:
                         self.myDate = (datetime.datetime.strptime(attr[1], '%B %d, %Y')
-                                                        .date()).strftime('%Y/%m/%d')
+                                                    .date()).strftime('%Y/%m/%d')
                         self.isDate = True
-                    except Exception as e:
-                        print("Error:", e)
+                except Exception as e:
+                    print("Error:", e)
+
     def handle_endtag(self, tag):
         """Handle the endtag in a website."""
         if tag == 'tr':
@@ -67,16 +75,19 @@ class WeatherScraper(HTMLParser):
                 self.inTr = False
             except Exception as e:
                 print("Error:", e)
+
         if tag == 'td':
             try:
                 self.inTd = False
             except Exception as e:
                 print("Error:", e)
+
         if tag == 'th':
             try:
                 self.inTh = False
             except Exception as e:
                 print("Error:", e)
+
         if tag == 'abbr':
             try:
                 self.inMean = False
@@ -87,38 +98,9 @@ class WeatherScraper(HTMLParser):
 
     def handle_data(self, data):
         """Handle the data inside a tag in a website and return dictionary."""
-        if data == 'Mean Temp':
-            try:
-                self.inAvg = True
-            except Exception as e:
-                print("Error:", e)
-        if data == 'Min Temp':
-            try:
-                self.inMin = True
-            except Exception as e:
-                print("Error:", e)
-        if data == 'Max Temp':
-            try:
-                self.inMax = True
-            except Exception as e:
-                print("Error:", e)
-        if self.inTd and self.i == 3 and self.inAvg:
-            try:
-                floatedMeanData = data
-                print("Mean  Inner:", floatedMeanData)
-                self.dictInner[self.keys[2]] = floatedMeanData
-            except Exception as e:
-                self.dictInner[self.keys[2]] = 0
-                print("Error:", e)
-        if self.inTd and self.i == 2 and self.inMin:
-            try:
-                floatedMinData = data
-                print("Min  Inner:", floatedMinData)
-                self.dictInner[self.keys[1]] = floatedMinData
-            except Exception as e:
-                self.dictInner[self.keys[1]] = 0
-                print("Error:", e)
-        if self.inTd and self.i == 1 and self.inMax:
+        if data == 'Sum':
+            self.inMyDate = False
+        if self.inMyDate and self.inTd and self.i == 1:
             try:
                 floatedMaxData = data
                 print("Max  Inner:", floatedMaxData)
@@ -127,13 +109,24 @@ class WeatherScraper(HTMLParser):
                 self.dictInner[self.keys[0]] = 0
                 print("Error:", e)
 
-        # this if statement will print only Avg value
-        # if self.inTd and self.i == 3 and self.inAvg:
-        #     try:
-        #         print("Avg  :", float(data))
-        #     except Exception as e:
-        #         print("Error:", e)
-        if self.isDate:
+        if self.inMyDate and self.inTd and self.i == 2:
+            try:
+                floatedMinData = data
+                print("Min  Inner:", floatedMinData)
+                self.dictInner[self.keys[1]] = floatedMinData
+            except Exception as e:
+                self.dictInner[self.keys[1]] = 0
+                print("Error:", e)
+
+        if self.inMyDate and self.inTd and self.i == 3:
+            try:
+                floatedMeanData = data
+                print("Mean  Inner:", floatedMeanData)
+                self.dictInner[self.keys[2]] = data
+            except Exception as e:
+                self.dictInner[self.keys[2]] = 0
+                print("Error:", e)
+        if self.inMyDate:
             self.dictOuter[self.myDate] = dict(self.dictInner)
 
 
@@ -154,9 +147,9 @@ now = datetime.datetime.now()
 #
 # myparser.feed(html)
 
-for i in range(now.year, 2018, -1):
+for i in range(now.year, 2017, -1):
     climateWeather_year = i
-    for j in range(12, 1, -1):
+    for j in range(12, 0, -1):
         climateWeather_month = j
         # climateWeather_url = 'https://climate.weather.gc.ca/'
         # cimateWeather_data = 'climate_data/daily_data_e.html?%20StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=%201&'
